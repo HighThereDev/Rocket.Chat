584331a118f49d54984a2c83158af171855cdd9c
Christian Moretti <christianmoretti@me.com>
2019-05-20 19:56:40 -0400

    Update environment variables

--- packages/rocketchat-lib/server/startup/settings.js
+++ packages/rocketchat-lib/server/startup/settings.js
@@ -2673,24 +2673,24 @@ RocketChat.settings.addGroup('Setup_Wizard', function() {
 
 RocketChat.settings.addGroup('Rate Limiter', function() {
 	this.section('DDP Rate Limiter', function() {
-		this.add('DDP_Rate_Limit_IP_Enabled', true, { type: 'boolean' });
+		this.add('DDP_Rate_Limit_IP_Enabled', false, { type: 'boolean' });
 		this.add('DDP_Rate_Limit_IP_Requests_Allowed', 120000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_IP_Enabled', value: true } });
-		this.add('DDP_Rate_Limit_IP_Interval_Time', 60000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_IP_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_IP_Interval_Time', 10000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_IP_Enabled', value: true } });
 
-		this.add('DDP_Rate_Limit_User_Enabled', true, { type: 'boolean' });
-		this.add('DDP_Rate_Limit_User_Requests_Allowed', 1200, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_Enabled', value: true } });
-		this.add('DDP_Rate_Limit_User_Interval_Time', 60000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_User_Enabled', false, { type: 'boolean' });
+		this.add('DDP_Rate_Limit_User_Requests_Allowed', 120000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_User_Interval_Time', 10000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_Enabled', value: true } });
 
-		this.add('DDP_Rate_Limit_Connection_Enabled', true, { type: 'boolean' });
-		this.add('DDP_Rate_Limit_Connection_Requests_Allowed', 600, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_Enabled', value: true } });
-		this.add('DDP_Rate_Limit_Connection_Interval_Time', 60000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_Connection_Enabled', false, { type: 'boolean' });
+		this.add('DDP_Rate_Limit_Connection_Requests_Allowed', 120000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_Connection_Interval_Time', 10000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_Enabled', value: true } });
 
-		this.add('DDP_Rate_Limit_User_By_Method_Enabled', true, { type: 'boolean' });
-		this.add('DDP_Rate_Limit_User_By_Method_Requests_Allowed', 20, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_By_Method_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_User_By_Method_Enabled', false, { type: 'boolean' });
+		this.add('DDP_Rate_Limit_User_By_Method_Requests_Allowed', 120000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_By_Method_Enabled', value: true } });
 		this.add('DDP_Rate_Limit_User_By_Method_Interval_Time', 10000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_User_By_Method_Enabled', value: true } });
 
-		this.add('DDP_Rate_Limit_Connection_By_Method_Enabled', true, { type: 'boolean' });
-		this.add('DDP_Rate_Limit_Connection_By_Method_Requests_Allowed', 10, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_By_Method_Enabled', value: true } });
+		this.add('DDP_Rate_Limit_Connection_By_Method_Enabled', false, { type: 'boolean' });
+		this.add('DDP_Rate_Limit_Connection_By_Method_Requests_Allowed', 120000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_By_Method_Enabled', value: true } });
 		this.add('DDP_Rate_Limit_Connection_By_Method_Interval_Time', 10000, { type: 'int', enableQuery: { _id: 'DDP_Rate_Limit_Connection_By_Method_Enabled', value: true } });
 	});
 
