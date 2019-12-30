bb50415c027a1ea76c844e9c658ef116d32617b4
JJ <jjsegado@unitedvirtualities.com>
2019-12-30 14:24:59 -0300

    enforcer add room participants, RC  doesn't update this!

--- app/api/server/helpers/composeRoomWithLastMessageCustomFields.js
+++ app/api/server/helpers/composeRoomWithLastMessageCustomFields.js
@@ -1,5 +1,6 @@
 import { normalizeMessagesForUserCustomFields } from '../../../utils/server/lib/normalizeMessagesForUserCustomFields';
 import { API } from '../api';
+import { Users, Subscriptions } from '../../../models/server';
 
 API.helperMethods.set('composeRoomWithLastMessageCustomFields', function _composeRoomWithLastMessageCustomFields(room, userId) {
 	if (room.lastMessage) {
@@ -11,6 +12,19 @@ API.helperMethods.set('composeRoomWithLastMessageCustomFields', function _compos
 		room.customFields.loc = null;
 		room.customFields.additional_data = null;
 	}
+
+	const subscriptions = Subscriptions.findByRoomId(room._id, {
+		fields: { 'u._id': 1 }
+	});
+
+	const members = subscriptions.fetch().map((s) => s.u && s.u._id);
+
+	const users = Users.find({ _id: { $in: members } }, {
+		fields: { _id: 1, username: 1, name: 1 }
+	}).fetch();
+
+	room.participants = users;
+
 
 	return room;
 });
